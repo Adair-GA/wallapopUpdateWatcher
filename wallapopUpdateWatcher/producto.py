dddbf0c4783409a7232338eb19bb5b469ca71aae
Adair-GA <adairyves@gmail.com>
2022-12-15 21:54:08 +0100

    añadido enlace a la clase producto, tests de la clase,  y cambiado el metodo para que no sonbreescriba __repr__

--- wallapopUpdateWatcher/producto.py
+++ wallapopUpdateWatcher/producto.py
@@ -9,17 +9,19 @@ class Producto:
     price: int
     shippable: bool
     city: str
+    link: str
 
     def __post_init__(self) -> None:
         if self.description is not None and len(self.description)>60:
             self.description = self.description[:60] + '...' 
 
+        self.link = f"https://es.wallapop.com/item/{self.link}"
 
 
-    def __repr__(self) -> str:
+    def msg(self) -> str:
         envio = "Si" if self.shippable else "No"
         
         if self.description is None:
-            return f"Producto: {self.title}.\nPrecio: {self.price}€. Se envia: {envio}"
+            return f"Producto: {self.title}.\nPrecio: {self.price}€. Se envia: {envio}.\n{self.link}"
         else:
-            return f"Producto: {self.title}. Descripción: {self.description}.\nPrecio: {self.price}€. Se envia: {envio}"
+            return f"Producto: {self.title}. Descripción: {self.description}.\nPrecio: {self.price}€. Se envia: {envio}.\n{self.link}"
